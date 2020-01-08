d32a9978aaecc2a85e0567121c4a94f79e20b71f
OMUR ALCIN (Bilgi Teknolojileri Baskanligi (Yazilim Gelistirme Md.) - Yuklenici-bt Personeli) <917962@thynet.thy.com>
2020-01-08 14:55:37 +0300

    path edited

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bike-ui-master'));
+app.use(express.static(__dirname + '/dist/bike-ui'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/bike-ui-master/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bike-ui/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
